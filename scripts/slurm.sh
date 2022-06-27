d3fa6681289d14e8130737098da6e2723c5172ae
Riccardo Orlando <orlandoricc@gmail.com>
2022-06-27 16:30:07 +0200

    Update time for SLURM

--- scripts/slurm.sh
+++ scripts/slurm.sh
@@ -2,11 +2,11 @@
 #SBATCH -A tra22_Nvaitc
 #SBATCH -p m100_sys_test
 #SBATCH -q qos_test
-#SBATCH --time 02:20:00     # format: HH:MM:SS
+#SBATCH --time 01:30:00     # format: HH:MM:SS
 #SBATCH -N 1                # 1 node
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
-#SBATCH --gres=gpu:1        # n gpus per node out of 4
+#SBATCH --gres=gpu:2        # n gpus per node out of 4
 #SBATCH --job-name=ner
 #SBATCH -o job.out          # for stdout redirection
 #SBATCH -e job.err          # for stderr redirection
@@ -14,4 +14,4 @@
 export WANDB_CACHE_DIR=$CINECA_SCRATCH/wandb_cache
 export HF_DATASETS_CACHE=$CINECA_SCRATCH/hf_datasets_cache
 
-srun ./scripts/train.sh "$@" "+model.model.local_files_only=True" "+data.dataset.local_files_only=True"
+srun ./scripts/train.sh "$@"
